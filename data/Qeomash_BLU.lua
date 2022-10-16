41a1080537be3f5f506a1b373bff4097fe18f402
Qeomash <qeomash@gmail.com>
2022-10-16 15:35:04 -0700

    BLU 99!

--- data/Qeomash_BLU.lua
+++ data/Qeomash_BLU.lua
@@ -15,54 +15,46 @@ end
 function init_gear_sets()
 
     sets.mainweapons = {
-        main="Firmament",
-        sub="Firmament",
+        main="Tokko Sword",
+        sub="Camaraderie Blade",
         ammo="Tiphia Sting",
     }
 
     sets.idle = {
         head="Magus Keffiyeh",
-        neck="Chivalrous Chain",
-        lear="Brutal Earring",
-        rear="Spike Earring",
         body="Mirage Jubbah",
-        hands="Magus Bazubands",
-        lring="Blood Ring",
-        rring="Rajas Ring",
-        back="Forager's Mantle",
-        waist="Swift Belt",
         legs="Crimson Cuisses",
-        feet="Crimson Greaves",
+        feet="Carmine Greaves", --haste+4%,STR+14,DA+3%
     }
 
     sets.engaged = {
-        head="Walahra Turban",
-        neck="Chivalrous Chain",
+        head="Wayfarer Circlet",
+        neck="Ocachi Gorget",
         lear="Brutal Earring",
         rear="Spike Earring",
-        body="Scorpion Harness",
-        hands="Dusk Gloves",
+        body="Weatherspoon Robe +1", --haste+3%,str+26
+        hands="Jhakri Cuffs", --Acc+25,Att+25
         lring="Blood Ring",
         rring="Rajas Ring",
         back="Forager's Mantle",
-        waist="Swift Belt",
-        legs="Homam Cosciales",
-        feet="Dusk Ledelsens",
+        waist="Dynamic Belt +1",
+        legs="Weatherspoon Pants +1",
+        feet="Carmine Greaves", --haste+4%,STR+14,DA+3%
     }
 
     sets.baseWS = {
-        head="Walahra Turban",
+        head="Wayfarer Circlet", --STR+19
         neck="Chivalrous Chain",
         lear="Brutal Earring",
-        rear="Spike Earring",
-        body="Assault Jerkin",
-        hands="Dusk Gloves",
-        lring="Blood Ring",
+        rear="Magnetic Earring",
+        body="Weatherspoon Robe +1", -- STR+26
+        hands="Jhakri Cuffs", --STR+10,Attack+25,WS+3%
+        lring="Flame Ring",
         rring="Rajas Ring",
         back="Forager's Mantle",
-        waist="Swift Belt",
-        legs="Mirage Shalwar",
-        feet="Dusk Ledelsens",
+        waist="Warwolf Belt",
+        legs="Weatherspoon Pants +1", -- STR+25
+        feet="Carmine Greaves", -- STR+14
     }
     sets.baseWS.Str = set_combine(sets.baseWS, {
         lring="Flame Ring",
@@ -74,7 +66,9 @@ function init_gear_sets()
 
     -- Spellcasting
     sets.precast.FC = {
-        lear="Loquacious Earring"
+        lear="Loquacious Earring",
+        back="Swith Cape",
+        feet="Carmine Greaves",
     }
 end
 
