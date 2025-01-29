fa30941c46bcddc5d19355322314fc7080408096
Qeomash <qeomash@gmail.com>
2025-01-28 23:57:59 -0800

    Updated Qeo RUN to level 99

--- data/Qeomash_RUN.lua
+++ data/Qeomash_RUN.lua
@@ -6,59 +6,87 @@ function get_sets()
 end
 
 function user_setup()
-    enable('main','sub','ranged', 'ammo')
+    send_command('gs enable all')
     select_default_macro_book()
     -- send_command('wait 2;input /lockstyleset 15')
     equip(sets.mainweapons)
+    send_command('wait 2;input /lockstyleset 30')
 end
 
 function init_gear_sets()
 
-    sets.mainweapons = {
-        main="Skofnung",
-        sub="Pole Grip",
+    OgmasCape = {}
+    OgmasCape.DD = {}
+    OgmasCape.Tank = {}
+    OgmasCape.WS = {}
+
+    sets.dpsweapons = {
+        main="Zantetsuken X",
+        sub="Utu Grip",
+    }
+    sets.tankingweapons = {
+        main="Aettir",
+        sub="Utu Grip",
+    }
+    sets.ddweapons = sets.dpsweapons
+    sets.mainweapons = sets.dpsweapons
+
+    sets.engagedDD = {
+        ammo="Coiste Bodhar",
+        head="Skormoth Mask",
+        neck="Anu Torque",
+        lear="Cessance Earring",
+        rear="Sherida Earring",
+        body="Ashera Harness",
+        hands="ayanmo Manopolas +2",
+        lring="Niqmaddu Ring",
+        rring="Epona's Ring",
+        back="Evasionist's Cape",
+        waist="Windbuffet Belt +1",
+        legs="Ayanmo Cosciales +2",
+        feet="Ayanmo Gambieras +2",
+    }
+    sets.engagedTank = {
+        ammo="Coiste Bodhar",
+        head="Nyame Helm",
+        neck="Loricate Torque +1",
+        lear="Cessance Earring",
+        rear="Erilaz Earring +1",
+        body="Nyame Mail",
+        hands="Nyame Gauntlets",
+        back="Evasionist's Cape",
+        waist="Flume Belt",
+        legs="Nyame Flanchard",
+        feet="Nyame Sollerets",
     }
 
-    sets.engaged = {
-        head="Walahra Turban",
-        neck="Peacock Charm",
-        lear="Suppanomimi",
-        rear="Brutal Earring",
-        body="Assault Jerkin",
-        hands="Dusk Gloves",
-        lring="Blood Ring",
-        rring="Rajas Ring",
-        back="Forager's Mantle",
-        waist="Swift Belt",
-        -- legs="Homam Cosciales",
-        feet="Dusk Ledelsens",
-    }
+    sets.engaged = sets.engagedTank
 
     sets.precast.FC = {
         lear="Loquacious Earring",
-        legs="Homam Cosciales",
+        legs="Ayanmo Cosciales +2",
+        feet="Carmine Greaves +1",
     }
 
-    sets.idle = set_combine(sets.engaged, {
-        hands="Hecatomb Mittens",
-        legs="Crimson Cuisses", --Movement+12%
-        feet="Hecatomb Leggings",
+    sets.idle = set_combine(sets.engagedTank, {
+        legs="Carmine Cuisses +1", --Movement+18%
     })
 
     sets.baseWS = {}
     sets.baseWS.Str = {
-        head="Hecatomb Cap",
-        neck="Peacock Charm",
-        lear="Suppanomimi",
-        rear="Brutal Earring",
-        body="Hecatomb Harness",
-        hands="Hecatomb Mittens",
-        lring="Blood Ring",
-        rring="Rajas Ring",
-        back="Forager's Mantle",
-        waist="Swift Belt",
-        legs="Hecatomb Subligar",
-        feet="Hecatomb Leggings",
+        ammo="Knobkierrie",
+        head="Nyame Helm",
+        neck="Fotia Gorget",
+        lear="Sherida Earring",
+        rear="Moonshade Earring", -- TPBonus+250
+        lear="Cessance Earring",
+        rear="Erilaz Earring +1",
+        body="Nyame Mail",
+        hands="Nyame Gauntlets",
+        back="Laic Mantle",
+        waist="Flume Belt",
+        legs="Nyame Flanchard",
+        feet="Nyame Sollerets",
     }
     sets.precast.WS = sets.baseWS.Str
 
